3aa13cd8a01452e71c75cbda57cc33f9c05038d8
Dmitriy Butakov <dmitry404@gmail.com>
2018-07-30 14:17:30 +0300

    groups API configuration

--- src/main/java/io/amberdata/ingestion/api/modules/stellar/client/IngestionApiClient.java
+++ src/main/java/io/amberdata/ingestion/api/modules/stellar/client/IngestionApiClient.java
@@ -1,37 +1,32 @@
 package io.amberdata.ingestion.api.modules.stellar.client;
 
-import java.util.function.Consumer;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import io.amberdata.domain.Block;
 import io.amberdata.domain.Transaction;
+import io.amberdata.ingestion.api.modules.stellar.configuration.IngestionApiProperties;
 
 import reactor.core.publisher.Mono;
 
 @Component
 public class IngestionApiClient {
 
-    private final WebClient webClient;
+    private final WebClient              webClient;
+    private final IngestionApiProperties apiProperties;
 
-    public IngestionApiClient (@Value("${ingestion.api.url}") String ingestionApiUrl) {
-        webClient = WebClient.builder()
-            .baseUrl(ingestionApiUrl)
+    public IngestionApiClient (IngestionApiProperties apiProperties) {
+        this.apiProperties = apiProperties;
+        this.webClient = WebClient.builder()
+            .baseUrl(apiProperties.getUrl())
             .defaultHeaders(this::configureApiHeaders)
             .build();
     }
 
     private void configureApiHeaders(HttpHeaders httpHeaders) {
-        httpHeaders.add("x-amberdata-blockchain-id", "f6d90419722d7691");
-        httpHeaders.add("x-amberdata-api-key", "0c866e124988b1bc994bbfb4e50a5289");
+        httpHeaders.add("x-amberdata-blockchain-id", apiProperties.getBlockchainId());
+        httpHeaders.add("x-amberdata-api-key", apiProperties.getApiKey());
     }
 
     public Mono<Block> publish (Block block) {
