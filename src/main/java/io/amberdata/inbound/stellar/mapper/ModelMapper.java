63ef567f30be416d4c4fb4fc74750f1d83fe85c2
Joshua Kwan <joshua.m.kwan@gmail.com>
2020-08-03 18:31:55 -0400

    better

--- src/main/java/io/amberdata/inbound/stellar/mapper/ModelMapper.java
+++ src/main/java/io/amberdata/inbound/stellar/mapper/ModelMapper.java
@@ -139,7 +139,6 @@ public class ModelMapper {
     // TODO: Memo is not serializable, will have to do it by hand
     // meta.put("memo",            transactionResponse.getMemo());
     meta.put("operation_count", transactionResponse.getOperationCount());
-    meta.put("max_fees", transactionResponse.getMaxFee());
 
     return new Transaction.Builder()
       .hash            (transactionResponse.getHash())
@@ -151,6 +150,7 @@ public class ModelMapper {
       .to              (to)
       .tos             (new ArrayList<>(tos))
       .fees            (BigDecimal.valueOf(transactionResponse.getFeeCharged()))
+      .gas             (BigInteger.valueOf(transactionResponse.getMaxFee()))
       .gasUsed         (BigInteger.valueOf(transactionResponse.getFeeCharged()))
       .numLogs         (transactionResponse.getOperationCount())
       .timestamp       (Instant.parse(transactionResponse.getCreatedAt()).toEpochMilli())
